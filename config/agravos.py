faa5c46fb4433639d5569ec30631a74cb3a47192
mbdev27 <maviaelbarroscontato@gmail.com>
2026-04-30 02:16:38 -0300

    Create agravos.py

---
+++ config/agravos.py
@@ -0,0 +1,9 @@
+AGRAVOS = {
+    "Acidente de Trabalho Grave": {
+        "slug": "acidente_trabalho_grave",
+        "pagina": "2_Painel_Acidente_Trabalho",
+        "mapping": "mappings.acidente_trabalho_grave",
+        "pdf": "assets/DRT_Acidente_Trabalho_Grave.pdf",
+        "descricao": "Ficha de investigação de Acidente de Trabalho Grave do SINAN."
+    }
+}
