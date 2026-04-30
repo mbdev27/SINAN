3c0edc81d78e74f06bad76550b25f1397fdf5f1d
mbdev27 <maviaelbarroscontato@gmail.com>
2026-04-30 03:10:52 -0300

    Change text color in tema.py

--- utils/tema.py
+++ utils/tema.py
@@ -34,7 +34,7 @@ def aplicar_tema_streamlit(st):
         }}
 
         html, body, [data-testid="stAppViewContainer"], * {{
-            color: #1b2642 !important;
+            color: #3a5391 !important;
         }}
 
         [data-testid="stAppViewContainer"] {{
