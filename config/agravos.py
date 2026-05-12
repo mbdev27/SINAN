dd0670cf57b67374971baaa7ec6cfc5f7cfcff00
mbdev27 <maviaelbarroscontato@gmail.com>
2026-05-12 19:11:25 -0300

    Update agravos.py

--- config/agravos.py
+++ config/agravos.py
@@ -1,25 +1,217 @@
 AGRAVOS = {
-    "Acidente de Trabalho Grave": {
-        "ficha": "DRT_Acidente_Trabalho_Grave.pdf"
+    "Acidente de Trabalho com Exposição a Material Biológico": {
+        "ficha": "ACBIONET.pdf"
     },
 
-    "Violência Interpessoal/Autoprovocada": {
-        "ficha": "Violência Interpessoal Autoprovocada.pdf"
+    "Acidente de Trabalho Grave": {
+        "ficha": "ACGRANET.pdf"
+    },
+
+    "Agravos Relacionados ao Trabalho": {
+        "ficha": "AGRAVNET.pdf"
+    },
+
+    "AIDS Adulto": {
+        "ficha": "AIDSANET.pdf"
+    },
+
+    "AIDS Criança": {
+        "ficha": "AIDSCNET.pdf"
+    },
+
+    "Acidente por Animais Peçonhentos": {
+        "ficha": "ANIMPNET.pdf"
+    },
+
+    "Atendimento Antirrábico Humano": {
+        "ficha": "ANTRANET.pdf"
+    },
+
+    "Botulismo": {
+        "ficha": "BOTUNET.pdf"
+    },
+
+    "Câncer Relacionado ao Trabalho": {
+        "ficha": "CANCENET.pdf"
+    },
+
+    "Doença de Chagas": {
+        "ficha": "CHAGANET.pdf"
+    },
+
+    "Chikungunya": {
+        "ficha": "CHIKON.pdf"
+    },
+
+    "Cólera": {
+        "ficha": "COLENET.pdf"
+    },
+
+    "Coqueluche": {
+        "ficha": "COQUENET.pdf"
+    },
+
+    "Dengue": {
+        "ficha": "DENGNET.pdf"
     },
 
     "Dengue/Chikungunya": {
-        "ficha": "1-DENGUE_2-CHIKUNGUNYA.pdf"
+        "ficha": "DENGUE_CHIKUNGUNYA.pdf"
+    },
+
+    "Dermatoses Ocupacionais": {
+        "ficha": "DERMANET.pdf"
+    },
+
+    "Difteria": {
+        "ficha": "DIFTENET.pdf"
+    },
+
+    "Doenças Transmitidas por Alimentos": {
+        "ficha": "DTANET.pdf"
+    },
+
+    "Epizootia": {
+        "ficha": "EPIZONET.pdf"
+    },
+
+    "Esporotricose": {
+        "ficha": "ESPORONET.pdf"
+    },
+
+    "Esquistossomose": {
+        "ficha": "ESQUINET.pdf"
+    },
+
+    "Doenças Exantemáticas": {
+        "ficha": "EXANTNET.pdf"
+    },
+
+    "Hanseníase": {
+        "ficha": "HANSNET.pdf"
+    },
+
+    "Hantavirose": {
+        "ficha": "HANTANET.pdf"
+    },
+
+    "Hepatites Virais": {
+        "ficha": "HEPANET.pdf"
+    },
+
+    "HIV Gestante": {
+        "ficha": "HIVGENET.pdf"
     },
 
     "Intoxicação Exógena": {
-        "ficha": "INTOXICAÇÃO EXÓGENA.pdf"
+        "ficha": "IEXOGNET.pdf"
+    },
+
+    "Influenza/SRAG": {
+        "ficha": "INFLUNET.pdf"
+    },
+
+    "Leishmaniose Visceral": {
+        "ficha": "LEISHNET.pdf"
     },
 
     "Leptospirose": {
-        "ficha": "LEPTOSPIROSE.pdf"
+        "ficha": "LEPTONET.pdf"
+    },
+
+    "LER/DORT": {
+        "ficha": "LERNET.pdf"
+    },
+
+    "Leishmaniose Tegumentar Americana": {
+        "ficha": "LTANET.pdf"
+    },
+
+    "Malária": {
+        "ficha": "MALANET.pdf"
+    },
+
+    "Meningite": {
+        "ficha": "MENINNET.pdf"
+    },
+
+    "Transtorno Mental Relacionado ao Trabalho": {
+        "ficha": "MENTANET.pdf"
+    },
+
+    "Notificação de Exposição a Contaminantes": {
+        "ficha": "NEXCNET.pdf"
+    },
+
+    "Febre do Nilo": {
+        "ficha": "NILONET.pdf"
+    },
+
+    "Notificação Individual": {
+        "ficha": "NINDINET.pdf"
+    },
+
+    "PAIR": {
+        "ficha": "PAIRNET.pdf"
+    },
+
+    "Peste": {
+        "ficha": "PESTENET.pdf"
+    },
+
+    "Paralisia Flácida Aguda": {
+        "ficha": "PFANET.pdf"
+    },
+
+    "Pneumoconioses": {
+        "ficha": "PNEUMNET.pdf"
+    },
+
+    "Raiva Humana": {
+        "ficha": "RAIVANET.pdf"
+    },
+
+    "Rotavírus": {
+        "ficha": "ROTANET.pdf"
+    },
+
+    "Sífilis em Gestante": {
+        "ficha": "SIFGENET.pdf"
+    },
+
+    "Sífilis Congênita": {
+        "ficha": "SIFICNET.pdf"
+    },
+
+    "Síndrome da Rubéola Congênita": {
+        "ficha": "SRCNET.pdf"
+    },
+
+    "Tétano Acidental": {
+        "ficha": "TETACNET.pdf"
+    },
+
+    "Tétano Neonatal": {
+        "ficha": "TETANNET.pdf"
+    },
+
+    "Tracoma": {
+        "ficha": "TRACONET.pdf"
+    },
+
+    "Tuberculose": {
+        "ficha": "TUBENET.pdf"
+    },
+
+    "Violência Interpessoal/Autoprovocada": {
+        "ficha": "VIOLENET.pdf"
+    },
+
+    "Zika": {
+        "ficha": "ZIKANET.pdf"
     },
 
     "Toxoplasmose": {
-        "ficha": "FICHA-DE-NOTIFICACAO-TOXOPLASMOSE.pdf"
+        "ficha": "TOXOPLASMOSE.pdf"
     },
 }
